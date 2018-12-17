d455c750f268f41328eaec3669aa89c1b5b8aa93
Gabriel Zavando <gabrielzavando@gmail.com>
2018-12-17 17:17:57 -0300

    termine la maquetacion del home

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ Template name: Plantilla para Inicio
             <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/globo.png')); ?>" >
           </div>
         </div>
-        <figcaption class="trasera" id="open1">
+        <figcaption class="trasera">
           <h2 class="card-rotate-title">
             Ecosistema
           </h2>
@@ -23,7 +23,7 @@ Template name: Plantilla para Inicio
           <p class="card-rotate-description">
             Diseñamos y ejecutamos programas que aceleran ecosistemas de emprendimiento e innovación.
           </p>
-          <div class="btn-modal">Ver más</div>
+          <a href="#Ecosistema">Ver más</a>
         </figcaption>
       </figure>
     </div>
@@ -36,7 +36,7 @@ Template name: Plantilla para Inicio
             <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/grafico.png')); ?>" >
           </div>
         </div>
-        <figcaption class="trasera" id="open2">
+        <figcaption class="trasera">
           <h2 class="card-rotate-title">
             Empresa
           </h2>
@@ -44,7 +44,7 @@ Template name: Plantilla para Inicio
           <p class="card-rotate-description">
             Asistimos la incorporación de técnicas de gestión de la innovación y manejamos portafolios de innovación empresarial.
           </p>
-          <div class="btn-modal">Ver más</div>
+          <a href="#Empresa" class="btn-mas">Ver más</a>
         </figcaption>
       </figure>
     </div>
@@ -57,7 +57,7 @@ Template name: Plantilla para Inicio
             <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/avion.png')); ?>" >
           </div>
         </div>
-        <figcaption class="trasera" id="open3">
+        <figcaption class="trasera">
           <h2 class="card-rotate-title">
             Innovacion abierta
           </h2>
@@ -65,85 +65,82 @@ Template name: Plantilla para Inicio
           <p class="card-rotate-description">
             Estamos en sintonía con la vanguardia de las nuevas técnicas y metodologías en gestión de la Innovación.
           </p>
-          <div class="btn-modal">Ver más</div>
+          <a href="#Innovacion" class="btn-mas">Ver más</a>
         </figcaption>
       </figure>
     </div>
   </div>
-  <div class="modal" id="modal1">
-    <section class="c-content-modal">
-      <div class="c-content-modal-img">
+  <div class="hacemos" id="Ecosistema">
+    <section class="c-content-hacemos">
+      <div class="c-content-hacemos-img">
         <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/hacemos1.jpg')); ?>">
         <figcaption>
           <h2>Ecosistema</h2>
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/globo.png')); ?>">
         </figcaption>
       </div>
-      <article class="c-content-modal-article">
-        <div class="c-content-modal-article-parrafos">
+      <article class="c-content-hacemos-article">
+        <div class="c-content-hacemos-article-parrafos">
           <p>Diseñamos y ejecutamos programas que aceleran ecosistemas de emprendimiento e innovación. Participamos en las etapas iniciales de definiciones estratégicas para el desarrollo de cada ecosistema y realizamos acciones de fortalecimiento y medición de su impacto.</p>
           <p>Trabajamos en red, sumando a organizaciones especialistas en cada área, complementando nuestras capacidades y nuestro método, ampliando así nuestra cobertura.</p>
           <p>Convocando a los actores claves, facilitamos el trabajo colaborativo entre los componentes de cada ecosistema. Nos mantenemos actualizados, gracias a la revisión permanente y estudio de nuevas metodologías y lecciones de referencia.</p>
           <p>Apoyamos a organizaciones gubernamentales, universidades, gremios y empresas que asumen el desafío de Innovar. En nuestro andar, hemos apoyado al desarrollo de diversos ecosistemas universitarios, atendiéndolos desde sus particularidades y las características de su entorno, creando e implementando diversos planes para potenciar sus capacidades de innovación y emprendimiento. Hemos acompañado también a gremios empresariales en su rol promotor del ecosistema.</p>
         </div>
-        <div class="c-content-modal-article-img">
+        <div class="c-content-hacemos-article-img">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/desafios.jpg')); ?>">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/flor.jpg')); ?>">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/aprendiendo.jpg')); ?>">
         </div>
       </article>
     </section>
-    <div class="closeModal" id="close1">&times;</div>
   </div>
-  <div class="modal" id="modal2">
-    <section class="c-content-modal">
-      <div class="c-content-modal-img">
+  <div class="hacemos" id="Empresa">
+    <section class="c-content-hacemos">
+      <div class="c-content-hacemos-img">
         <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/hacemos2.jpg')); ?>">
         <figcaption>
           <h2>Empresa</h2>
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/grafico.png')); ?>">
         </figcaption>
       </div>
-      <article class="c-content-modal-article">
-        <div class="c-content-modal-article-parrafos">
+      <article class="c-content-hacemos-article">
+        <div class="c-content-hacemos-article-parrafos">
           <p>Asistimos la incorporación de técnicas de gestión de la innovación y manejamos portafolios de innovación empresarial. Nuestros servicios apoyan los diseños estratégicos, así como la habilitación de técnicas de administración y cambios en la cultura de la innovación. Además, potenciamos las iniciativas innovadoras a través de asesorías, añadiendo de igual forma la mirada de portafolio y las técnicas de gestión de este tipo de carteras de proyectos.</p>
           <p>Vinculamos a las empresas con emprendimientos, fuentes de conocimientos como Universidades y Centros de Investigación, de manera de sumar estas capacidades técnicas a las demandas de cada iniciativa.</p>
           <p>Apoyamos en la búsqueda de financiamiento público y privado, brindamos métodos y conexiones para que las empresas puedan validar sus iniciativas y posicionarlas en el mercado, sin necesidad de descuidar su día a día.</p>
         </div>
-        <div class="c-content-modal-article-img">
+        <div class="c-content-hacemos-article-img">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/desafios.jpg')); ?>">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/flor.jpg')); ?>">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/aprendiendo.jpg')); ?>">
         </div>
       </article>
     </section>
-    <div class="closeModal" id="close2">&times;</div>
   </div>
-  <div class="modal" id="modal3">
-    <section class="c-content-modal">
-      <div class="c-content-modal-img">
+  <div class="hacemos" id="Innovacion">
+    <section class="c-content-hacemos">
+      <div class="c-content-hacemos-img">
         <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/hacemos3.jpg')); ?>">
         <figcaption>
           <h2>Innovacion abierta</h2>
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/avion.png')); ?>">
         </figcaption>
       </div>
-      <article class="c-content-modal-article">
-        <div class="c-content-modal-article-parrafos">
+      <article class="c-content-hacemos-article">
+        <div class="c-content-hacemos-article-parrafos">
           <p>Estamos en sintonía con la vanguardia de las nuevas técnicas y metodologías en gestión de la Innovación. Es así que uno de nuestros objetivos como empresa es generar nuevos lazos y componentes para las dinámicas del innovar.</p>
           <p>En la Innovación Abierta las empresas, a través de la declaración de sus desafíos, interactúan y realizan trabajo colaborativo con su entorno, aprovechando así las capacidades allí contenidas, tales como, emprendedores, científicos, centros de desarrollo de conocimientos, universitarios y Pymes.</p>
           <p>Este modelo de gestión ofrece a las empresas ahorros en los esfuerzos de innovación, fortalece las redes del ecosistema territorial y a su vez propicia relaciones sostenibles con sus colaboradores.</p>
           <p>Creamos y ejecutamos programas de Innovación Abierta. Trabajamos con las empresas, desde la detección y definición de sus desafíos, apoyándoles en las etapas de búsqueda y selección de solucionadores. Capacitamos a empresas y a solucionadores para fortalecer y hacer más efectiva su vinculación.</p>
           <p>Hemos creado nuestros propios métodos, acumulando amplia experiencia a través de los programas que hemos realizado con diversas empresas e industrias en distintos territorios de Chile y Latinoamérica.</p>
         </div>
-        <div class="c-content-modal-article-img">
+        <div class="c-content-hacemos-article-img">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/desafios.jpg')); ?>">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/flor.jpg')); ?>">
           <img src="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/aprendiendo.jpg')); ?>">
         </div>
       </article>
     </section>
-    <div class="closeModal" id="close3">&times;</div>
   </div>
 </main>
 <?php get_footer(); ?>
