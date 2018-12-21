b247f9366ba0b98ec215d31741d7112ff961f92e
Gabriel Zavando <gabrielzavando@gmail.com>
2018-12-21 17:55:08 -0300

    Modifiqué la automatización del sitio para trabajar mejor

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,95 +1,69 @@
-'use strict'
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import plumber from 'gulp-plumber';
+import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import jsmin from 'gulp-jsmin';
+import imagemin from 'gulp-imagemin';
 
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const bust = require('gulp-cache-bust');
-const watch = require('gulp-watch');
-const plumber = require('gulp-plumber');
-const gulpsass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCss = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const concat = require('gulp-concat');
-const jshint = require('gulp-jshint');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const livereload = require('gulp-livereload');
+const reload = browserSync.reload,
+  reloadFiles = [
+    './script.js',
+    './style.css',
+    './**/*.php'
+  ],
+  proxyOptions = {
+    proxy: 'localhost:8080/Quintil/',
+    notify: false
+  },
+  imagenminOptions = {
+    progressive: true,
+    optimizationLevel: 3, //van de 0 a 7
+    interlaced: true,
+    svgoPlugins: [{removeViewBox: false}]
+  }
 
-let onError = function(err){
-  console.log('Se ha producido un error: ', err.message);
-  this.emit('end');
-}
+gulp.task('server', () => browserSync.init(reloadFiles, proxyOptions))
 
-gulp.task('cache', () => {
-	gulp.src('*.php')
-    .pipe(bust({
-        type: 'timestamp'
-    }))
-    .pipe(gulp.dest('.'));
+gulp.task('css', () => {
+  gulp.src('./css/style.scss')
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer({browsers: ['last 2 versions']}))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./css/'))
+    .pipe(gulp.dest('./'))
+    .pipe(reload({stream: true}))
 });
 
-gulp.task('sass', () => {
-  return gulp.src('./scss/style.scss')
-    .pipe(plumber({errorHandler: onError}))
-    // Iniciamos el trabajo con sourcemaps
-    .pipe(sourcemaps.init())
-    .pipe(gulpsass())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('.'))
-    .pipe(cleanCss({keepSpecialComments: 1}))
-    // Escribir los sourcemaps
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('.'))
-    .pipe(livereload())
+// gulp.task('js', () => {
+//   browserify('./js/index.js')
+//     .transform(babelify)
+//     .bundle()
+//     .on('error', err => console.log(err.message))
+//     .pipe(source('script.js'))
+//     .pipe(buffer())
+//     .pipe(sourcemaps.init({loadMaps: true}))
+//     .pipe(sourcemaps.write('./js/'))
+//     .pipe(jsmin())
+//     .pipe(gulp.dest('./'))
+//     .pipe(reload({stream: true}))
+// });
+
+gulp.task('img', () => {
+  gulp.src('./img/raw/**/*.{png,jpg,jpeg,gif,svg}')
+    .pipe(imagemin(imagenminOptions))
+    .pipe(gulp.dest('./img/'))
 });
 
-gulp.task('lint', () => {
-	return gulp.src('./js/**/*.js')
-		.pipe(jshint())
-});
-
-gulp.task('globaljs', ['lint'], function() {
-	return gulp.src('./js/globales/**.js')
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-		.pipe(concat('global.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('./js'))
-		.pipe(livereload())
-});
-
-gulp.task('uniquejs', ['lint'], function() {
-	return gulp.src('./js/unicos/**.js')
-    .pipe(plumber({ errorHandler: onError }))
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-		.pipe(uglify())
-		.pipe(gulp.dest('./js'))
-		.pipe(livereload())
-});
-
-gulp.task('imagemin', () => {
-	return gulp.src('./img/dev/**.*')
-		.pipe(plumber({ errorHandler: onError }))
-		.pipe(imagemin({
-			progressive: true,
-			interlaced: true
-		}))
-		.pipe(gulp.dest('./img/'))
-		.pipe(livereload())
-});
-
-gulp.task('watch', function () {
-	livereload.listen();
-	gulp.watch('./scss/**/**.scss', ['sass', 'cache'])
-	gulp.watch('./js/**/**.js', ['globaljs', 'uniquejs', 'cache'])
-	gulp.watch('./img/dev/**.*', ['imagemin'])
-
-});
-
-gulp.task('default', ['sass', 'globaljs', 'uniquejs', 'imagemin', 'watch'], function(){
-
+gulp.task('default', ['server', 'css'], () => {
+  gulp.watch('./scss/**/*.scss', ['css'])
+  // gulp.watch('./js/**/*.js', ['js'])
 });
--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
 <head>
-  <title><?php wp_title('', true, 'right');?></title>
-  <meta charset="<?php bloginfo( 'charset' )?>" />
+  <title><?php bloginfo('name');?></title>
+  <meta charset="<?php bloginfo( 'charset' );?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <meta name="description" content="<?php bloginfo('description') ?>" />
+  <meta name="description" content="<?php bloginfo('description'); ?>" />
   <link rel="icon" type="image/x-icon" href="<?php echo esc_url(home_url('/wp-content/themes/quintil/img/favicon.png')); ?>" />
 <?php wp_head();?>
 </head>
